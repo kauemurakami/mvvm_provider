16a7aa3d31d4fe7c8c2d0c4417b902710862d3c4
Kauê <kauetmurakami@gmail.com>
2025-01-15 21:14:02 -0300

    update packages to latest versions

--- lib/constants/my_theme_data.dart
+++ lib/constants/my_theme_data.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 class MyThemeData {
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    cardColor: Colors.white30,
     appBarTheme: const AppBarTheme(backgroundColor: Colors.white, foregroundColor: Colors.black, elevation: 1),
     colorScheme: const ColorScheme.light(
       surface: Color.fromARGB(26, 13, 184, 247),
     ),
   );
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    cardColor: Colors.white70,
     appBarTheme: AppBarTheme(
       backgroundColor: Colors.grey.shade800,
       foregroundColor: Colors.white,
